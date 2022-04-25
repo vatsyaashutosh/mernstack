83c7b762fcd2ee63715e6cb87b4e68d4c60b1e1a
vatsyaashutosh <96139619+vatsyaashutosh@users.noreply.github.com>
2022-04-25 20:01:01 +0530

    pagination filteration and search is done

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use(express.json());
 
+
 const product = require("./routes/productRoute");
 
 app.use("/api/v1", product);
--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,73 +1,100 @@
 const Product = require("../models/productModel");
+const ApiFeatures = require("../utils/apifeatures");
 
 // Create Product --Admin
 exports.createProduct = async (req, res, next) => {
-  const product = await Product.create(req.body);
-  res.status(201).json({
-    success: true,
-    product,
-  });
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    res.status(404).json(error);
+  }
 };
 //Get all product
 exports.getAllProducts = async (req, res) => {
-  const products = await Product.find();
+  try {
+    const resultPerPage = 5;
+    const productCount = await Product.countDocuments();
+    const apiFeature = new ApiFeatures(Product.find(), req.query)
+      .search()
+      .filter()
+      .pagination(resultPerPage);
+    const products = await apiFeature.query;
 
-  res.status(200).json({ success: true, products });
+    res.status(200).json({ success: true, products, productCount });
+  } catch (error) {
+    res.status(404).json(error);
+  }
 };
 
 //Get product by id
 
 exports.getProductDetails = async (req, res, next) => {
-  const product = await Product.findById(req.params.id);
-  if (!product) {
-    return res.status(500).json({
-      success: false,
-      message: "Product not found",
+  try {
+    let product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(500).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      product,
     });
+  } catch (error) {
+    res.status(404).json(error);
   }
-  res.status(200).json({
-    success: true,
-    product,
-  });
 };
 
 // Update Product --Admin
 
 exports.updateProduct = async (req, res, next) => {
-  let product = await Product.findById(req.params.id);
-  if (!product) {
-    return res.status(500).json({
-      success: false,
-      message: "Product not found",
+  try {
+    let product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(500).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
     });
+
+    res.status(200).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    res.status(404).json(error);
   }
-
-  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-    useFindAndModify: false,
-  });
-
-  res.status(200).json({
-    success: true,
-    product,
-  });
 };
 
 // Delete --admin(
 
 exports.deleteProduct = async (req, res, next) => {
-  let product = await Product.findById(req.params.id);
-  if (!product) {
-    return res.status(500).json({
-      success: false,
-      message: "Product not found",
+  try {
+    let product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(500).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+    product = await Product.findByIdAndDelete(req.params.id, { new: true });
+    res.status(200).json({
+      success: true,
+      message: "Product deleted successfully",
+      product,
     });
+  } catch (error) {
+    res.status(404).json(error);
   }
-  product = await Product.findByIdAndDelete(req.params.id, { new: true });
-  res.status(200).json({
-    success: true,
-    message: "Product deleted successfully",
-    product,
-  });
 };
